ca49cee76576b0ebb84e56d91fe2117033ea549d
darkoeller <darko.eller@gmail.com>
2023-12-21 10:20:06 +0100

    Add project files.

---
+++ Ispit.Data/Models/TodoListContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Ispit.Data.Models;
+
+public class TodoListContext : DbContext
+{
+   public TodoListContext()
+   {
+
+   }
+
+   public TodoListContext(DbContextOptions<TodoListContext> options) : base(options)
+   {
+
+   }
+
+   public DbSet<TodoList> TodoLists { get; set; }
+   protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+   {
+      base.OnConfiguring(optionsBuilder);
+      if (!optionsBuilder.IsConfigured)
+      {
+         optionsBuilder.UseSqlServer("Data Source=DESKTOP-DBUENO3;Initial Catalog=TodoListDb;Integrated Security=True;Encrypt=False; Trust Server Certificate=True");
+      }
+   }
+
+   protected override void OnModelCreating(ModelBuilder modelBuilder)
+   {
+      base.OnModelCreating(modelBuilder);
+      //modelBuilder.Entity<TodoList>().HasData(
+      //);
+   }
+}

1054e814555acee67b5c0d832098d4778c847e56
darkoeller <darko.eller@gmail.com>
2023-12-21 10:23:22 +0100

    KreiranaBaza

---
+++ Ispit.Data/Migrations/20231221092255_Initial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ispit.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TodoLists",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsCompleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoLists", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TodoLists");
+        }
+    }
+}
--- Ispit.Data/Models/TodoList.cs
+++ Ispit.Data/Models/TodoList.cs
@@ -1,8 +1,13 @@
-﻿namespace Ispit.Data.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ispit.Data.Models
 {
    public class TodoList
    {
+      [Key]
       public int Id { get; set; }
+      [Required]
+      [StringLength(50)]
       public string Title { get; set; } = string.Empty;
       public string Description { get; set; } = string.Empty;
       public bool IsCompleted { get; set; }
